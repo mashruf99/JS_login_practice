88db05658251afc9e977f2952e72a89c132b5da8
Shafiul islam mashruf <bemashrufislam@gmail.com>
2025-05-18 00:09:12 +0600

    index

--- js/login.js
+++ js/login.js
@@ -2,11 +2,17 @@ console.log("login.js loaded");
 
 const button = document.getElementById("button-login");
 const phoneNumber = document.getElementById("phone-number");
+const pinNumber = document.getElementById("pin-number");
+
 button.addEventListener("click", function (event) {
     event.preventDefault();
     console.log("Button clicked");
     console.log(phoneNumber.value);  
+    console.log(pinNumber.value);
+    if (phoneNumber.value === "017897352088" || pinNumber.value === "1234") {
+        console.log("Login successful");
+        window.location.href = "home.html";
+    }
+
 });
 
-
-        
